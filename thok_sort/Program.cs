82718d4a02e9fa5fec40416c8c6ff4a23c00a3c0
Lusunping <l.sunping@gami.com>
2016-08-05 16:18:15 +0800

    add database conn

--- thok_sort/Program.cs
+++ thok_sort/Program.cs
@@ -15,7 +15,7 @@ namespace thok_sort
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new thok_sort());
         }
     }
 }
--- thok_sort/thok_sort.cs
+++ thok_sort/thok_sort.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace thok_sort
 {
-    public partial class Form1 : Form
+    public partial class thok_sort : Form
     {
-        public Form1()
+        public thok_sort()
         {
             InitializeComponent();
         }
